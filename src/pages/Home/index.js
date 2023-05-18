8ad4506a4a11d36c6203981de6e2b7784ab60e55
Murilo Carvalho <90083333+dev-muca@users.noreply.github.com>
2023-05-17 23:24:32 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,20 +19,6 @@ function App() {
     <div className="flex flex-col justify-center items-center bg-zinc-100">
       {nutri.map((post) => (
         <Card key={post.id} id={post.id} titulo={post.titulo} subtitulo={post.subtitulo} capa={post.capa} />
-        // <article key={post.id} className="flex flex-col justify-center w-[700px] shadow rounded p-4 my-8">
-        //   <strong className="text-xl mb-4 w-full text-center">{post.titulo}</strong>
-
-        //   <img src={post.capa} alt={post.titulo} />
-
-        //   <p className="m-4 text-justify">{post.subtitulo}</p>
-
-        //   <a
-        //     className="bg-none rounded text-blue-500 border  border-blue-500 p-2 flex justify-center items-center font-bold"
-        //     href={"/post/" + (post.id - 1)}
-        //   >
-        //     Ler sobre
-        //   </a>
-        // </article>
       ))}
     </div>
   );
